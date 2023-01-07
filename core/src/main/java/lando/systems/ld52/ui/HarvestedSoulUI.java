6a168f12a981d69d2bbfd706f7c1e19730bdf9a0
Jeffrey Hwang <jinsoo.jeffrey.hwang@gmail.com>
2023-01-07 17:47:47 -0600

    removed death ui on bottom left

--- core/src/main/java/lando/systems/ld52/ui/HarvestedSoulUI.java
+++ core/src/main/java/lando/systems/ld52/ui/HarvestedSoulUI.java
@@ -8,6 +8,8 @@ import lando.systems.ld52.Assets;
 
 public class HarvestedSoulUI extends VisWindow {
 
+    VisLabel occupationHeaderLabel;
+    VisLabel funFactHeaderLabel;
     VisLabel nameLabel;
     VisLabel occupationLabel;
     VisLabel funFactLabel;
@@ -17,29 +19,33 @@ public class HarvestedSoulUI extends VisWindow {
         setTouchable(Touchable.disabled);
         setBackground(Assets.Patch.metal.drawable);
         align(Align.top);
-        nameLabel = new VisLabel("Elon Musk", "large");
+        nameLabel = new VisLabel("", "large");
         add(nameLabel).align(Align.center);
         row();
-        VisLabel label = new VisLabel("Occupation: ");
-        label.setFontScale(1f);
-        add(label).align(Align.left);;
+        occupationHeaderLabel = new VisLabel("Occupation: ");
+        occupationHeaderLabel.setFontScale(1f);
+        occupationHeaderLabel.setVisible(false);
+        add(occupationHeaderLabel).align(Align.left);;
         row();
-        occupationLabel = new VisLabel("Professional Tweeter");
+        occupationLabel = new VisLabel("");
         occupationLabel.setFontScale(.75f);
         add(occupationLabel).align(Align.left);
         row();
-        label = new VisLabel("Fun Facts:");
-        label.setFontScale(1f);
-        add(label).align(Align.left);;
+        funFactHeaderLabel = new VisLabel("Fun Facts:");
+        funFactHeaderLabel.setFontScale(1f);
+        funFactHeaderLabel.setVisible(false);
+        add(funFactHeaderLabel).align(Align.left);;
         row();
-        funFactLabel = new VisLabel("Father of X Æ A-12");
+        funFactLabel = new VisLabel("");
         funFactLabel.setFontScale(.75f);
         add(funFactLabel).align(Align.left).growX();
     }
 
     public void setSoul(String name, String occupation, String funFact) {
         nameLabel.setText(name);
+        occupationHeaderLabel.setVisible(true);
         occupationLabel.setText(occupation);
+        funFactHeaderLabel.setVisible(true);
         funFactLabel.setText(funFact);
     }
 }
--- core/src/main/java/lando/systems/ld52/ui/LeftSideUI.java
+++ core/src/main/java/lando/systems/ld52/ui/LeftSideUI.java
@@ -1,26 +1,25 @@
 package lando.systems.ld52.ui;
 
+import com.badlogic.gdx.utils.Align;
 import com.kotcrab.vis.ui.widget.VisTable;
 import lando.systems.ld52.Assets;
 
 public class LeftSideUI extends VisTable {
     private final float WIDTH = 256f;
     private final float SCOREBOX_HEIGHT = 80f;
-    private final float CHARACTER_STAT_HEIGHT = 250f;
+    private final float CHARACTER_STAT_HEIGHT = 256f;
     private final float MARGIN = 12f;
     public ScoreBoxUI scoreBoxUI;
     public HarvestedSoulUI harvestedSoulUI;
-    public PlayerStatUI playerStatUI;
     public LeftSideUI(Assets assets, float x, float y, float width, float height) {
         setSize(width, height);
         setPosition(x, y);
+        align(Align.top);
         scoreBoxUI = new ScoreBoxUI();
         add(scoreBoxUI).pad(MARGIN).growX().size(WIDTH, SCOREBOX_HEIGHT);
         row();
         harvestedSoulUI = new HarvestedSoulUI();
         add(harvestedSoulUI).padLeft(MARGIN).padRight(MARGIN).padBottom(MARGIN).growX().size(WIDTH, CHARACTER_STAT_HEIGHT);
         row();
-        playerStatUI = new PlayerStatUI(assets);
-        add(playerStatUI).padLeft(MARGIN).padRight(MARGIN).padBottom(MARGIN).growX().width(WIDTH).growY();
     }
 }
