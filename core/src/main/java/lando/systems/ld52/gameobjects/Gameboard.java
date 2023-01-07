694cae36030bf26cac665cfe6353073c750bcdcc
Doug Graham <dsgraham@gmail.com>
2023-01-06 21:10:32 -0600

    encapsulate more into the tile class

--- core/src/main/java/lando/systems/ld52/gameobjects/Gameboard.java
+++ core/src/main/java/lando/systems/ld52/gameobjects/Gameboard.java
@@ -17,26 +17,27 @@ public class Gameboard {
     public Gameboard() {
         boardArea = new Rectangle(Config.Screen.window_width/2f - boardWidth/2f, Config.Screen.window_height/2f - boardWidth/2f, boardWidth, boardWidth);
         tiles = new Tile[gridSize][];
+        float gridDelta = boardWidth / gridSize;
         for (int x = 0; x < gridSize; x++){
             tiles[x] = new Tile[gridSize];
             for (int y = 0; y < gridSize; y++) {
-                tiles[x][y] = new Tile(x, y);
+                tiles[x][y] = new Tile(x, y, gridDelta, boardArea);
             }
         }
     }
 
     public void update(float dt) {
-
+        for (int x = 0; x < gridSize; x++) {
+            for (int y = 0; y < gridSize; y++) {
+                tiles[x][y].update(dt);
+            }
+        }
     }
 
     public void render(SpriteBatch batch) {
-        float gridDelta = boardWidth / gridSize;
         for (int x = 0; x < gridSize; x++){
             for (int y = 0; y < gridSize; y++) {
-                Tile t = tiles[x][y];
-                batch.setColor(t.tileColor);
-                batch.draw(Main.game.assets.pixelRegion, boardArea.x + (x*gridDelta), boardArea.y + (y * gridDelta), gridDelta, gridDelta);
-                batch.setColor(Color.WHITE);
+                tiles[x][y].render(batch);
             }
         }
     }
--- core/src/main/java/lando/systems/ld52/gameobjects/Tile.java
+++ core/src/main/java/lando/systems/ld52/gameobjects/Tile.java
@@ -1,20 +1,35 @@
 package lando.systems.ld52.gameobjects;
 
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+import lando.systems.ld52.Main;
 
 public class Tile {
 
     public int x;
     public int y;
+    public Rectangle tileArea;
 
     // Debug probably
     public Color tileColor;
 
-    public Tile(int x, int y) {
+    public Tile(int x, int y, float tileSize, Rectangle board) {
         this.x = x;
         this.y = y;
+        tileArea = new Rectangle(board.x + x * tileSize, board.y + y * tileSize, tileSize, tileSize);
         tileColor = new Color(Color.WHITE);
         tileColor.fromHsv(MathUtils.random(360f), .5f, .8f);
+    }
+
+    public void update(float dt) {
+
+    }
+
+    public void render(SpriteBatch batch) {
+        batch.setColor(tileColor);
+        batch.draw(Main.game.assets.pixelRegion, tileArea.x, tileArea.y, tileArea.width, tileArea.height);
+        batch.setColor(Color.WHITE);
     }
 }
