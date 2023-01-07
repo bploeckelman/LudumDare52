f886c02aef744ac2f44ca83730b47b3f959e40f7
Jeffrey Hwang <jinsoo.jeffrey.hwang@gmail.com>
2023-01-07 15:25:50 -0600

    remove hourglass placeholder

--- core/src/main/java/lando/systems/ld52/ui/GameScreenUI.java
+++ core/src/main/java/lando/systems/ld52/ui/GameScreenUI.java
@@ -1,6 +1,5 @@
 package lando.systems.ld52.ui;
 
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.kotcrab.vis.ui.widget.VisTable;
 import lando.systems.ld52.Assets;
@@ -10,7 +9,6 @@ public class GameScreenUI extends VisTable {
     public RightSideUI rightSideUI;
     public GameScreenUI(OrthographicCamera windowCamera, Assets assets) {
         setFillParent(true);
-        setColor(Color.RED);
         setSize(windowCamera.viewportWidth, windowCamera.viewportHeight);
         setPosition(0, 0);
         leftSideUI = new LeftSideUI(assets, 0, 0, 280f, windowCamera.viewportHeight);
--- core/src/main/java/lando/systems/ld52/ui/RightSideUI.java
+++ core/src/main/java/lando/systems/ld52/ui/RightSideUI.java
@@ -1,21 +1,20 @@
 package lando.systems.ld52.ui;
 
+import com.badlogic.gdx.utils.Align;
 import com.kotcrab.vis.ui.widget.VisTable;
 import lando.systems.ld52.Assets;
 
 public class RightSideUI extends VisTable {
     private final float WIDTH = 256f;
-    private final float HOURGLASS_HEIGHT = 240f;
+    private final float QUOTA_HEIGHT = 450f;
     private final float MARGIN = 12f;
     public QuotaListUI quotaListUI;
     public HourGlassUI hourGlassUI;
     public RightSideUI(Assets assets, float x, float y, float width, float height) {
+        align(Align.top);
         setSize(width, height);
         setPosition(x, y);
         quotaListUI = new QuotaListUI(assets);
-        add(quotaListUI).pad(MARGIN).growX().growY().width(WIDTH);
-        row();
-        hourGlassUI = new HourGlassUI();
-        add(hourGlassUI).padLeft(MARGIN).padRight(MARGIN).padBottom(MARGIN).size(WIDTH, HOURGLASS_HEIGHT);
+        add(quotaListUI).pad(MARGIN).growX().size(WIDTH, QUOTA_HEIGHT);
     }
 }
