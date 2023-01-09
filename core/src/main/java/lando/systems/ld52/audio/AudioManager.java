ad3e18eb0a8677b6de0b377f96a6d31a193351a1
Pete V <petervaleo@gmail.com>
2023-01-09 15:07:57 -0600

    Reverting volume change

--- core/src/main/java/lando/systems/ld52/audio/AudioManager.java
+++ core/src/main/java/lando/systems/ld52/audio/AudioManager.java
@@ -86,8 +86,8 @@ public class AudioManager implements Disposable {
 
 //        putMusic(Musics.mainTheme, assets.settingSound);
 
-        musicVolume = new MutableFloat(0.6f);
-        soundVolume = new MutableFloat(0.7f);
+        musicVolume = new MutableFloat(0.7f);
+        soundVolume = new MutableFloat(0.9f);
 
         isMusicMuted = false;
         isSoundMuted = false;
